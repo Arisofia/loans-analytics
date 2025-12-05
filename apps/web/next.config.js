eb533b462f6fadf86b1dddc0cbf692204ff50643
9nnxqzyq4y-eng <9nnxqzyq4y@privaterelay.appleid.com>
2025-12-05 10:41:33 -0600

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -31,7 +31,6 @@ const nextConfig = {
   // eslint-disable-next-line @typescript-eslint/require-await
   rewrites: async () => ({
     beforeFiles: [],
-    afterFiles: [],
     fallback: [],
   }),
 };
