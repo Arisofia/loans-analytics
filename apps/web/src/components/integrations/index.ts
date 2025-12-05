86616f527ed1218d1f5128b15e3d3b0b389f60d3
9nnxqzyq4y-eng <9nnxqzyq4y@privaterelay.appleid.com>
2025-12-05 11:01:44 -0600

    Create index.ts

---
+++ apps/web/src/components/integrations/index.ts
@@ -0,0 +1,13 @@
+// Export main integration components for barrel import
+export { default as IntegrationSettings } from './IntegrationSettings';
+export { default as SlideShell } from './SlideShell';
+export { default as IntegrationCard } from './IntegrationCard';
+export { default as BulkTokenInput } from './BulkTokenInput';
+export { default as SlideLayout } from './SlideLayout';
+export { default as FluidBackground } from './FluidBackground';
+
+// Export types
+export type { StatusRow } from './IntegrationSettings';
+export type * from './IntegrationCard';
+export type * from './SlideShell';
+export type * from './SlideLayout';
