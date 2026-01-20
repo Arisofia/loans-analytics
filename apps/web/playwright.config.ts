b764422f11cf2d86441ab2cfa165cc025ed4cc4b
Arisofia <jenineferderas@hotmail.com>
2026-01-20 11:40:49 +0100

    Update playwright.config.ts for Playwright configuration

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,15 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env.local') });
-
-export default defineConfig({
-  testDir: './e2e',
-  fullyParallel: true,
-  reporter: 'html',
-=======
 
 export default defineConfig({
   testDir: './tests/e2e',
@@ -18,7 +7,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'line',
->>>>>>> fix/workflows-lint-fix2
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
@@ -26,18 +14,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-<<<<<<< HEAD
-      use: { ...devices['Desktop Chrome'], storageState: 'playwright/.auth/user.json' },
-    },
-  ],
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
-=======
       use: { ...devices['Desktop Chrome'] },
     },
   ],
->>>>>>> fix/workflows-lint-fix2
 });
