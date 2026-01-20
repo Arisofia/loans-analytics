79b8c35279758e7062152c92f877431a3e132c34
Arisofia <jenineferderas@hotmail.com>
2026-01-21 00:14:59 +0100

    chore: remove next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Turbopack is disabled by default when 'experimental' is removed
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
